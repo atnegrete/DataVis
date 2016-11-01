07a93fbea3509554317dbc1cb5ab06282ae05941
tbyacoub <tbyacoub@gmail.com>
2016-10-31 19:38:49 -0500

    some bar-graph-data-form work was done

--- app/bar-graph/bar-graph.component.js
+++ app/bar-graph/bar-graph.component.js
@@ -35,45 +35,16 @@ angular
                 }
             };
 
-            this.data = [
-                {
-                    key: "Cumulative Return",
-                    values: [
-                        {
-                            "label" : "A" ,
-                            "value" : -29.765957771107
-                        } ,
-                        {
-                            "label" : "B" ,
-                            "value" : 0
-                        } ,
-                        {
-                            "label" : "C" ,
-                            "value" : 32.807804682612
-                        } ,
-                        {
-                            "label" : "D" ,
-                            "value" : 196.45946739256
-                        } ,
-                        {
-                            "label" : "E" ,
-                            "value" : 0.19434030906893
-                        } ,
-                        {
-                            "label" : "F" ,
-                            "value" : -98.079782601442
-                        } ,
-                        {
-                            "label" : "G" ,
-                            "value" : -13.925743130903
-                        } ,
-                        {
-                            "label" : "H" ,
-                            "value" : -5.1387322875705
-                        }
-                    ]
-                }
-            ]
+            this.data = [{key: "Cumulative Return", values: []}];
 
+            this.update = function (d) {
+                this.data[0]["values"].push(angular.copy(d));
+                d.value = null;
+                d.label = null;
+            };
+
+            this.edit = function (td) {
+              console.log(td[parent]);
+            };
         }
     });
