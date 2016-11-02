e2384d73ad488b0729a80693b2ad8c96ba0a4fe4
tbyacoub <tbyacoub@gmail.com>
2016-11-01 20:56:55 -0500

    Completed bar-graph

--- app/bar-graph/bar-graph.component.js
+++ app/bar-graph/bar-graph.component.js
@@ -5,8 +5,8 @@ angular
         templateUrl: 'bar-graph/bar-graph.template.html',
         controller: function barGraphController(){
 
-            this.x_axis_name = "X axis";
-            this.y_axis_name = "Y axis";
+            this.x_axis_label = "X axis";
+            this.y_axis_label = "Y axis";
 
             this.options = {
                 chart: {
@@ -22,14 +22,14 @@ angular
                     y: function(d){return d.value;},
                     showValues: true,
                     valueFormat: function(d){
-                        return d3.format(',.4f')(d);
+                        return d3.format(",.4f")(d);
                     },
                     duration: 500,
                     xAxis: {
-                        axisLabel: this.x_axis_name
+                        axisLabel: this.x_axis_label
                     },
                     yAxis: {
-                        axisLabel: this.y_axis_name,
+                        axisLabel: this.y_axis_label,
                         axisLabelDistance: -10
                     }
                 }
@@ -37,14 +37,43 @@ angular
 
             this.data = [{key: "Cumulative Return", values: []}];
 
-            this.update = function (d) {
-                this.data[0]["values"].push(angular.copy(d));
+            this.update = function(d) {
+                if(d == undefined) return;
+                if(!d.label.match(/^[0-9a-zA-Z]+$/)) return;
+                if(d.value == undefined) return;
+                var edited = false;
+                var i;
+                data = this.data[0]['values'];
+                for(i = 0; i < data.length; i++) {
+                    if(data[i]["label"] == d.label) {
+                        data[i]["value"] = d.value;
+                        edited = true;
+                    }
+                }
+
+                if(!edited) {
+                    data.push(angular.copy(d));
+                }
                 d.value = null;
                 d.label = null;
             };
 
-            this.edit = function (td) {
-              console.log(td[parent]);
+            this.refreshLabels = function(){
+                this.options["chart"]["xAxis"]["axisLabel"] = this.x_axis_label;
+                this.options["chart"]["yAxis"]["axisLabel"] = this.y_axis_label;
+            };
+
+            this.sort = function(st){
+                if (st == undefined) return;
+                data = this.data[0]['values'];
+                data.sort(function(a, b){
+                    if (st == "ascending") {
+                        return parseFloat(a.value) - parseFloat(b.value);
+                    } else {
+                        return parseFloat(b.value) - parseFloat(a.value);
+                    }
+                });
+                st = null;
             };
         }
     });
